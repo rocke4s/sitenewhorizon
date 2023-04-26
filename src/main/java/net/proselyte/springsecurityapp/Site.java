7c1cdc49539f5b20255490a4992af1f604cf9422
rocke4s <128725620+rocke4s@users.noreply.github.com>
2023-04-26 09:17:00 +0500

    Новая БД

---
+++ src/main/java/net/proselyte/springsecurityapp/Site.java
@@ -0,0 +1,12 @@
+//package net.proselyte.springsecurityapp;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//
+//@SpringBootApplication
+//public class Site {
+//    public static void main(String[] args) {
+//        SpringApplication.run(Site.class, args);
+//    }
+//
+//}
