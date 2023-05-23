2516ed1849730ce2b9e5353b695d18b2407c2185
rocke4s <128725620+rocke4s@users.noreply.github.com>
2023-05-23 11:56:02 +0500

    (4) 22.05.2023

--- src/main/java/net/proselyte/springsecurityapp/config/SecurityConfig.java
+++ src/main/java/net/proselyte/springsecurityapp/config/SecurityConfig.java
@@ -1,20 +1,11 @@
 package net.proselyte.springsecurityapp.config;
 
-import net.proselyte.springsecurityapp.service.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableWebSecurity
@@ -43,8 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutSuccessUrl("/login?logout")
                 .and()
-                .csrf()
-                .ignoringAntMatchers("/statuser");
+                .csrf().disable();
     }
 
 //        @Autowired
